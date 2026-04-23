741efeebe5526fe82b0b399b266b58e5a3d607f2
Murilo Rocha <murilorocharegatieri1703@gmail.com>
2026-04-23 19:08:22 -0300

    refactor: remove camadas antigas do versionamento e finaliza clean architecture

--- backend/services/livro_service.py
+++
@@ -1,9 +0,0 @@
-from repositories.livro_repository import LivroRepository
-
-repo = LivroRepository()
-
-def listar_livros(query=None, preco_min=None):
-    return repo.listar(query, preco_min)
-
-def buscar_por_id(id):
-    return repo.buscar_por_id(id)
